20aef8759f1b9ceb78e3f0af25bbf3dc7b7f2f5f
chocomint <nhi01202446047@gmail.com>
2025-01-14 23:49:30 -0800

    add noti

--- lib/views/pages/nguyen.dart
+++ lib/views/pages/nguyen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NguyenScreen extends StatefulWidget {
   const NguyenScreen({Key? key}) : super(key: key);
@@ -10,11 +11,18 @@ class NguyenScreen extends StatefulWidget {
 class _NguyenScreenState extends State<NguyenScreen> with TickerProviderStateMixin {
   late TabController _tabController;
   TimeOfDay _selectedTime = TimeOfDay.now();
+  late FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings =
+    InitializationSettings(android: initializationSettingsAndroid);
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
   Future<void> _selectTime(BuildContext context) async {
@@ -26,7 +34,28 @@ class _NguyenScreenState extends State<NguyenScreen> with TickerProviderStateMix
       setState(() {
         _selectedTime = picked;
       });
+      _scheduleNotification(picked);
     }
+  }
+
+  Future<void> _scheduleNotification(TimeOfDay time) async {
+    final now = DateTime.now();
+    final selectedDateTime = DateTime(now.year, now.month, now.day, time.hour, time.minute);
+    final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+    final platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+    await _flutterLocalNotificationsPlugin.schedule(
+      0,
+      'Time Alert',
+      'The selected time has arrived!',
+      selectedDateTime,
+      platformChannelSpecifics,
+    );
   }
 
   @override
